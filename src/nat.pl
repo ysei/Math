03ff71bc16ba141b03d09e3e0415259f3ae878a5
Sebastián <sreguera@gmail.com>
2011-11-01 23:29:58 +0100

    Added comments, pow and conversions.

--- src/nat.pl
+++ src/nat.pl
@@ -16,23 +16,78 @@
 */
 :- module(nat, []).
 
+
+%% nat(N)
+%
+% True if N is a Nat. A Nat is a natural number represented as either 0
+% or s(N) where N is a Nat.
+
 nat(0).
 nat(s(N)) :-
         nat(N).
 
+
+%% sum(N, M, P)
+%
+% P is the sum of N and M.
+
 sum(0, M, M) :-
         nat(M).
-sum(s(N), M, s(R)) :-
-        sum(N, M, R).
+sum(s(N), M, s(P)) :-
+        sum(N, M, P).
+
+
+%% prod(N, M, P)
+%
+% P is the product of N and M.
 
 prod(0, M, 0) :-
         nat(M).
 prod(s(N), M, P) :-
-        prod(N, M, P0),
-        sum(M, P0, P).
+        prod(N, M, Q),
+        sum(M, Q, P).
+
+
+%% pow(N, M, P)
+%
+% P is M to the power of N.
+
+pow(0, M, s(0)) :-
+        nat(M).
+pow(s(N), M, P) :-
+        pow(N, M, Q),
+        prod(M, Q, P).
+
+
+%% less_eq(N, M)
+%
+% N is less than or equal to M.
 
 less_eq(N, M) :-
         sum(N, _, M).
+
+
+%% nat_to_number(+Nat, -Number) is det.
+%
+% Convert a Nat to a prolog number.
+
+nat_to_number(0, 0).
+nat_to_number(s(N), X) :-
+        nat_to_number(N, Y),
+        X is Y + 1.
+
+
+%% number_to_nat(+Number, -Nat) is semidet.
+%
+% Convert a prolog number to a Nat. Fails if Number is not a natural number.
+
+number_to_nat(0, 0) :-
+        !.
+number_to_nat(X, s(N)) :-
+        X > 0,
+        Y is X - 1,
+        number_to_nat(Y, N).
+
 
 :- begin_tests(nat).
 
