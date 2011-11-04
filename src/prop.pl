67de363c828eb256bcba14c6701c01face7d1831
Sebastián <sreguera@gmail.com>
2011-11-04 20:24:31 +0100

    Added prop.pl file about propositional logic.

---
+++ src/prop.pl
@@ -0,0 +1,135 @@
+/*
+  Copyright 2011 Jose Sebastian Reguera Candal
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+:- module(prop, []).
+
+
+%% prop(Proposition)
+%
+% True if Proposition is a well formed propositional formula.
+
+prop(eq(P, Q)) :-
+        prop(P),
+        prop(Q).
+prop(imp(P, Q)) :-
+        prop(P),
+        prop(Q).
+prop(and(P, Q)) :-
+        prop(P),
+        prop(Q).
+prop(or(P, Q)) :-
+        prop(P),
+        prop(Q).
+prop(not(P)) :-
+        prop(P).
+prop(prop(P)) :-
+        atom(P).
+prop(0).
+prop(1).
+
+
+%% eval(+Proposition, +Environment, -Value)
+%
+% Evaluate the Proposition in Environmnent giving Value as a result.
+% - Proposition is as defined in the predicate "prop" above.
+% - Environment is a list of pairs where the first element of the pair is
+% a proposition name and the second its associated value (0 or 1). For
+% example: [p-1, q-0]
+% - Value is the resulting value (0 or 1).
+
+eval(eq(P, Q), Env, V) :-
+        eval(P, Env, PV),
+        eval(Q, Env, QV),
+        eval_eq(PV, QV, V).        
+eval(imp(P, Q), Env, V) :-
+        eval(P, Env, PV),
+        eval(Q, Env, QV),
+        eval_imp(PV, QV, V).
+eval(and(P, Q), Env, V) :-
+        eval(P, Env, PV),
+        eval(Q, Env, QV),
+        eval_and(PV, QV, V).
+eval(or(P, Q), Env, V) :-
+        eval(P, Env, PV),
+        eval(Q, Env, QV),
+        eval_or(PV, QV, V).
+eval(not(P), Env, V) :-
+        eval(P, Env, PV),
+        eval_not(PV, V).
+eval(prop(P), Env, V) :-
+        memberchk(P-V, Env).
+eval(0, _, 0).
+eval(1, _, 1).
+
+
+% eval_op(+P, +Q, -V)
+% eval_op(+P, -V)
+%
+% Helper functions that use cuts for avoiding the indeterminacy that would
+% result for using the op tables directly.
+
+eval_eq(P, Q, V) :-
+        eq(P, Q, V),
+        !.
+eval_imp(P, Q, V) :-
+        imp(P, Q, V),
+        !.
+eval_and(P, Q, V) :-
+        and(P, Q, V),
+        !.
+eval_or(P, Q, V) :-
+        or(P, Q, V),
+        !.
+eval_not(P, V) :-
+        not(P, V),
+        !.
+
+
+% op(P, Q, V)
+% op(P, V)
+%
+% Truth tables for the logical operators.
+
+eq(0, 0, 1).
+eq(0, 1, 0).
+eq(1, 0, 0).
+eq(1, 1, 1).
+
+imp(0, 0, 1).
+imp(0, 1, 1).
+imp(1, 0, 0).
+imp(1, 1, 1).
+
+and(0, 0, 0).
+and(0, 1, 0).
+and(1, 0, 0).
+and(1, 1, 1).
+
+or(0, 0, 0).
+or(0, 1, 1).
+or(1, 0, 1).
+or(1, 1, 1).
+
+not(0, 1).
+not(1, 0).
+
+
+:- begin_tests(prop).
+
+test(or) :-
+        eval(or(prop(p), prop(q)), [p-1, q-0], 1).
+
+:- end_tests(prop).
