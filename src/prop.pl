2770c967e0f7d364902dc4829f6f099ceabf16fd
Sebastián <sreguera@gmail.com>
2011-11-05 15:36:34 +0100

    Added vars predicate and renamed 'prop' functor to 'var'.

--- src/prop.pl
+++ src/prop.pl
@@ -35,10 +35,41 @@ prop(or(P, Q)) :-
         prop(Q).
 prop(not(P)) :-
         prop(P).
-prop(prop(P)) :-
+prop(var(P)) :-
         atom(P).
 prop(0).
 prop(1).
+
+
+%% vars(+Prop, -Vars)
+%
+% Vars is a sorted list of the propositional variables in Prop.
+
+vars(eq(P, Q), V) :-
+        vars(P, VP),
+        vars(Q, VQ),
+        ord_union(VP, VQ, V).
+vars(imp(P, Q), V) :-
+        vars(P, VP),
+        vars(Q, VQ),
+        ord_union(VP, VQ, V).
+vars(and(P, Q), V) :-
+        vars(P, VP),
+        vars(Q, VQ),
+        ord_union(VP, VQ, V).
+vars(or(P, Q), V) :-
+        vars(P, VP),
+        vars(Q, VQ),
+        ord_union(VP, VQ, V).
+vars(not(P), V) :-
+        vars(P, V).
+vars(var(P), V) :-
+        ord_empty(V0),
+        ord_add_element(V0, P, V).
+vars(0, V) :-
+        ord_empty(V).
+vars(1, V) :-
+        ord_empty(V).
 
 
 %% eval(+Proposition, +Environment, -Value)
@@ -46,8 +77,8 @@ prop(1).
 % Evaluate the Proposition in Environmnent giving Value as a result.
 % - Proposition is as defined in the predicate "prop" above.
 % - Environment is a list of pairs where the first element of the pair is
-% a proposition name and the second its associated value (0 or 1). For
-% example: [p-1, q-0]
+% a propositional variable name and the second its associated value (0 or 1).
+% For example: [p-1, q-0]
 % - Value is the resulting value (0 or 1).
 
 eval(eq(P, Q), Env, V) :-
@@ -69,7 +100,7 @@ eval(or(P, Q), Env, V) :-
 eval(not(P), Env, V) :-
         eval(P, Env, PV),
         eval_not(PV, V).
-eval(prop(P), Env, V) :-
+eval(var(P), Env, V) :-
         memberchk(P-V, Env).
 eval(0, _, 0).
 eval(1, _, 1).
@@ -130,6 +161,6 @@ not(1, 0).
 :- begin_tests(prop).
 
 test(or) :-
-        eval(or(prop(p), prop(q)), [p-1, q-0], 1).
+        eval(or(var(p), var(q)), [p-1, q-0], 1).
 
 :- end_tests(prop).
